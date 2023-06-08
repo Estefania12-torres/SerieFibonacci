af551e3a3558fcf4f18ff99d2709d2e313bc6b86
Estefania12-torres <133797333+Estefania12-torres@users.noreply.github.com>
2023-06-08 11:01:33 -0500

    Update Arbol.java

--- src/Arbol.java
+++ src/Arbol.java
@@ -1,2 +1,16 @@
 package PACKAGE_NAME;public class Arbol {
+  
+    public Node raiz;
+    public Node dato;
+
+    public Arbol(int dato) {
+        this.dato = new Node(dato);
+    }
+
+    public void agregarDato(int dato) {
+    }
+
+    public String toString() {
+        return "Arbol" + this.raiz.dato;
+    }
 }
