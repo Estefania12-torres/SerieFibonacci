5fae6fd31c62a3b351c661c8fa95eaadc155d6db
Abel214 <131829158+Abel214@users.noreply.github.com>
2023-06-12 20:28:09 -0500

    Update Serie.java
    Cambios en la clase

--- src/Serie.java
+++ src/Serie.java
@@ -2,34 +2,31 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.Scanner;
 import java.util.Iterator;
-public class Serie {
-    //La  clase llamado valorList que es un mapa (HashMap) que se utilizará para almacenar los valores calculados de la serie.
+public class Serie { //Se usa un almacén de valores para la serie
+   
     private Map<Integer, Long> valorList;
     public Serie() {
         this.valorList = new HashMap<>();
     }
-    //calculo del valor de Fibonacci para una posición dada. Si el valor ya está calculado y almacenado en el mapa, se devuelve ese valor.
-    public long getFibonacci(int position) {
+    
+    public long getFibonacci(int position) { //Permite el calculo del valor de Fibonacci para una posición dada
         if (position == 0 || position == 1) {
             return 1;
-        } else {
-            // Verifica si ya se ha calculado y almacena el valor para la posición dada en valorList. valorList es un 
-       
+        } else { //Al verficarse en el ciclo almacena el valor para la posición dada en valorList
             if (valorList.containsKey(position)) {
                 return valorList.get(position);
             }
             // Si el valor no está en "valorList", se calcula el número de "Fibonacci" para la posición dada utilizando 
            
             long resultado = getFibonacci(position - 1) + getFibonacci(position - 2);
-            //Conservacion del resultado calculado en "valorList" para futuras referencias y cálculos.
             valorList.put(position, resultado);
             return resultado;
             }
 
         }
 
-    public void calcularFibonacciConScanner() {
-            Scanner scanner = new Scanner(System.in);
+    public void calcularFibonacciConScanner() { 
+            Scanner scanner = new Scanner(System.in); //Crea una instancia de la clase 
 
             System.out.print("Ingrese la posición de Fibonacci: ");
             int posicion = scanner.nextInt();
