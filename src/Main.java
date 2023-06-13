ee5238a3c1610460a2ef3356f2e6790529e3083d
Brian Fernando Aguinsaca Loarte <133794609+BrianFernandoAguinsacaLoarte@users.noreply.github.com>
2023-06-12 20:59:53 -0500

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,5 @@
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
-public class Main {
-    public static void main(String[] args) {
-      Serie serie = new Serie();
-      System.out.println(serie.getFibonacci(50));
-      System.out.println(serie);
-    }
-}
+//Implementacion de las importaciones 
+import java.util.Map;
+import java.util.Scanner;
